c65f2f30725f9aa7b854aeaca82c8b14bff514dc
Hiro-Nagai <gm.komabun2003@gmail.com>
2022-08-17 14:08:00 +0000

    basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,23 @@
 class UsersController < ApplicationController
- def show
-    @user = User.find(params[:id])
-    # debugger
-    
-    # user = User.find(params[:id])　と @userの違い@はインスタンス　なしはローカルそこでしか使えない
- end
- 
- def new
-    @user = User.new
- end  
 
- def create
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
- end
+  end
 
   private
 
